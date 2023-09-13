f3302338e7d62cbc6065cfd55f04c55abe0bf6c7
Saira Dee <sairadee@pop-os.localdomain>
2023-09-13 09:15:28 +0200

    New Commit

---
+++ src/main/java/org/example/Greeting/WebbyHelloWorld.java
@@ -0,0 +1,12 @@
+package org.example.Greeting;
+
+import io.javalin.Javalin;
+
+public class WebbyHelloWorld {
+    public static void main(String[] args) {
+        Javalin server = Javalin.create()
+                .start(7000);
+        server.get("hello",
+                context -> context.result("Hello, world!"));
+    }
+}
